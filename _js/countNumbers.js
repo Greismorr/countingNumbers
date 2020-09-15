26f085564515df0c0f6faea5b7af5927c88e7480
stackOvercrow <gabriel-reis1999@hotmail.com>
2020-09-15 16:26:37 -0300

    First commit, completely functional counter

---
+++ _js/countNumbers.js
@@ -0,0 +1,29 @@
+function countNumbers(){
+
+    /*Using numberInput = Number(window.document.querySelector('input#numberInput').value) would make an empty input be an 
+    acceptable value. But allow empty inputs wasn't my objective on this site. */
+
+    const numberInput = window.document.querySelector('input#numberInput').value
+    const incrementInput = (window.document.querySelector('input#incrementNumber').value)
+    const targetInput = (window.document.querySelector('input#targetNumber').value)
+    const showCount = window.document.querySelector('p#showCount')
+
+    var numberToIncrement = Number(window.document.querySelector('input#numberInput').value)
+    var incrementNumber = Number(window.document.querySelector('input#incrementNumber').value)
+    var targetNumber = Number(window.document.querySelector('input#targetNumber').value)
+
+    if(numberInput === '' || incrementInput === '' || targetInput === '' || numberToIncrement > targetNumber){ 
+        alert("Por favor, preencha todos os dados corretamente.\nNote que o número inicial deve sempre ser menor que o final.")
+    }else{
+        showCount.innerHTML = ''
+
+        while(numberToIncrement < targetNumber){
+            numberToIncrement += incrementNumber
+
+            if(numberToIncrement > targetNumber){
+                numberToIncrement = 10
+            }
+            showCount.innerHTML += '👉' + numberToIncrement + ' '
+        }
+    }
+}
